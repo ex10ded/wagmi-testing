d206d2c62a0a44baa90b30889e556cee9b6d78b6
stefano <stefano.franz@me.com>
2024-03-06 17:03:13 +0000

    add login logic

--- wagmi-testing/src/App.tsx
+++ wagmi-testing/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const { disconnect } = useDisconnect()
   const { signTypedDataAsync } = useSignTypedData()
 
-  const [text, setText] = useState(`
+  const [registerText, setRegisterText] = useState(`
   {
     "domain":{
        "name":"ex10.exchange"
@@ -52,28 +52,79 @@ function App() {
        "tosAccepted":true,
        "time":"${time.toISOString()}"
     }
- }
-  `);
-  const [signature, setSignature] = useState("")
+ }`);
+
+  const [loginText, setLoginText] = useState(`
+ {
+   "domain":{
+      "name":"ex10.exchange"
+   },
+   "primaryType":"Login",
+   "types":{
+      "EIP712Domain":[
+         {
+            "name":"name",
+            "type":"string"
+         }
+      ],
+      "Login":[
+         {
+            "name":"host",
+            "type":"string"
+         },
+         {
+            "name":"action",
+            "type":"string"
+         },
+         {
+            "name":"time",
+            "type":"string"
+         }
+      ]
+   },
+   "message":{
+      "host":"${window.location.hostname}",
+      "action":"LOGIN",
+      "time":"${time.toISOString()}"
+   }
+}`);
+
+  const [registerSignature, setRegisterSignature] = useState("")
   const [starkKey, setStarkKey] = useState("")
   const [pedersenHash, setPedersenHash] = useState("")
   const [starkSignature, setStarkSignature] = useState("")
-  const [payload, setPayload] = useState("")
+  const [registerPayload, setRegisterPayload] = useState("")
 
+  const [loginSignature, setLoginSignature] = useState("")
+  const [loginPayload, setLoginPayload] = useState("")
 
-  function handleInput(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    console.log(JSON.stringify(event.target.value))
-    setText(event.target.value)
+  async function login() {
+    setLoginSignature("")
+    let obj = JSON.parse(loginText)
+    // step 1 obtain signature from wallet over typed data
+    let l1Signature = await signTypedDataAsync(obj)
+
+    let payload = {
+      l1Signature: l1Signature,
+      login: {
+        host: window.location.hostname,
+        action: "LOGIN",
+        time: time.toISOString()
+      }
+    }
+
+    setLoginSignature(l1Signature)
+    setLoginPayload(JSON.stringify(payload, null, 2))
   }
 
-  async function signData() {
-    setSignature("")
-    let obj = JSON.parse(text)
+  async function register() {
+    setRegisterSignature("")
+    let obj = JSON.parse(registerText)
     console.log(obj)
     // step 1 obtain signature from wallet over typed data
     let l1Signature = await signTypedDataAsync(obj)
     console.log(l1Signature)
-    setSignature(l1Signature)
+    setRegisterSignature(l1Signature)
     // step 2 derive starkKeys from l1 signature
     let starkPrivate = keyDerivation.getPrivateKeyFromEthSignature(l1Signature)
     let starkPublic = keyDerivation.privateToStarkKey(starkPrivate)
@@ -90,10 +141,10 @@ function App() {
     // step 5 build POST payload for /auth/register
     let payload = {
       l1Signature: l1Signature,
-      l2Key: "0x"+ starkPublic,
+      l2Key: "0x" + starkPublic,
       l2Signature: {
-        r: "0x" + l2Signature.r,
-        s: "0x" + l2Signature.s,
+        r: "0x" + l2Signature.r.toJSON(),
+        s: "0x" + l2Signature.s.toJSON(),
       },
       accountCreation: {
         accountIndex: 0,
@@ -103,7 +154,7 @@ function App() {
       }
     }
 
-    setPayload(JSON.stringify(payload, null, 2))
+    setRegisterPayload(JSON.stringify(payload, null, 2))
   }
 
   return (
@@ -143,59 +194,91 @@ function App() {
 
       <div>
 
-        <h2>Sign</h2>
-        <textarea
-          placeholder="Set Message"
-          maxLength={2000}
-          onChange={handleInput}
-          value={text}
-          rows={50}
-          cols={120}
-        />
-        <br></br>
+        <div className="row" >
+          <div className="column">
+
+            <h2>Register</h2>
+            <textarea
+              placeholder="Set Message"
+              maxLength={2000}
+              onChange={(event) => setRegisterText(event.target.value)}
+              value={registerText}
+              rows={50}
+              cols={120}
+            />
+            <br></br>
 
 
 
-        <button
-          onClick={async () => await signData()}
-        >
-          Sign message
-        </button>
+            <button
+              onClick={async () => await register()}
+            >
+              Register
+            </button>
 
-        <br></br>
-        <textarea
-          value={signature}
-          rows={2}
-          cols={140}
-        ></textarea>
+            <br></br>
+            <textarea
+              value={registerSignature}
+              rows={2}
+              cols={140}></textarea>
 
-        <br></br>
-        <textarea
-          value={starkKey}
-          rows={2}
-          cols={140}
-        ></textarea>
+            <br></br>
+            <textarea
+              value={starkKey}
+              rows={2}
+              cols={140}></textarea>
 
-        <br></br>
-        <textarea
-          value={pedersenHash}
-          rows={2}
-          cols={140}
-        ></textarea>
+            <br></br>
+            <textarea
+              value={pedersenHash}
+              rows={2}
+              cols={140}></textarea>
 
-        <br></br>
-        <textarea
-          value={starkSignature}
-          rows={2}
-          cols={140}
-        ></textarea>
+            <br></br>
+            <textarea
+              value={starkSignature}
+              rows={2}
+              cols={140}></textarea>
 
-        <br></br>
-        <textarea
-          value={payload}
-          rows={30}
-          cols={160}
-        ></textarea>
+            <br></br>
+            <textarea
+              value={registerPayload}
+              rows={30}
+              cols={140}></textarea>
+
+          </div>
+          <div className="column">
+            <h2>Login</h2>
+            <textarea
+              placeholder="Set Message"
+              maxLength={2000}
+              onChange={(event) => setLoginText(event.target.value)}
+              value={loginText}
+              rows={50}
+              cols={120} />
+
+            <br></br>
+            <button
+              onClick={async () => await login()}
+            >
+              Login
+            </button>
+            <br></br>
+            <textarea
+              value={loginSignature}
+              rows={2}
+              cols={140}></textarea>
+
+            <textarea
+              value={loginPayload}
+              rows={30}
+              cols={140}></textarea>
+
+            <br></br>
+          </div>
+        </div>
+
+
 
       </div>
     </>
